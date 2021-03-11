139b5093cd213d0993bcb1a0ff18eb080ecf0592
Alexandr <va-front@fulogy.com>
2021-03-11 23:43:32 +0300

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+import s from '../styles/app.module.sass'
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <p>
+    <p className={s.p} >
       <Link href="/about">
         <a>About</a>
       </Link>
